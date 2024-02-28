1ff908776abb05692b6b097840741635f75e4dd3
Damir Hasanshin <d.xasanshin@gmail.com>
2024-02-28 19:27:02 +0300

    add button

--- app/src/main/java/com/example/l2/MainActivity.kt
+++ app/src/main/java/com/example/l2/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.l2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val redBtn = findViewById<Button>(R.id.button)
+
+        redBtn?.setOnClickListener() {
+            Toast.makeText(this@MainActivity, R.string.red_btn, Toast.LENGTH_LONG).show()
+        }
+
+        val greenBtn = findViewById<Button>(R.id.button2)
+
+        greenBtn?.setOnClickListener() {
+            Toast.makeText(this@MainActivity, R.string.green_btn, Toast.LENGTH_LONG).show()
+        }
     }
 }
